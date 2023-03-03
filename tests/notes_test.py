09b3c9bffdbd5b21c3f78983c13025794a9d7c76
Victor Angelo <vic.angelo19@gmail.com>
2023-03-03 15:58:15 -0300

    notes.py completely refactored using TDD and OOP.

--- tests/notes_test.py
+++ tests/notes_test.py
@@ -113,3 +113,65 @@ if __name__ == "__main__":
             all_about_note()
         if decision == "B":
             break
+
+
+class TestNoteClass:
+    note_natural = Note("D")
+    note_sharp = Note("D#")
+    note_flat = Note("Db")
+
+    def test_is_accidental(self):
+        assert self.note_natural.is_accidental() is False
+        assert self.note_sharp.is_accidental() is True
+        assert self.note_flat.is_accidental() is True
+
+    def test_get_accident_sign(self):
+        self.note_sharp.get_accident_sign()
+        self.note_flat.get_accident_sign()
+        assert self.note_sharp.accident_sign == "#"
+        assert self.note_flat.accident_sign == "b"
+
+    def test_is_flat(self):
+        assert self.note_natural.is_flat() is False
+        assert self.note_sharp.is_flat() is False
+        assert self.note_flat.is_flat() is True
+
+    def test_is_sharp(self):
+        assert self.note_natural.is_sharp() is False
+        assert self.note_sharp.is_sharp() is True
+        assert self.note_flat.is_sharp() is False
+
+    def test_enharmonize(self):
+        enharmonized_sharp = self.note_sharp.enharmonize()
+        enharmonized_flat = self.note_flat.enharmonize()
+        assert enharmonized_sharp.name == "Eb"
+        assert enharmonized_flat.name == "C#"
+
+
+class TestChromaticScaleGeneratorClass:
+    note_sharp = Note("D#")
+    note_flat = Note("Db")
+    sharp_chroma_gen = ChromaticScaleGenerator(note_sharp)
+    flat_chroma_gen = ChromaticScaleGenerator(note_flat)
+
+    def test_generate_base_chromatic_scale(self):
+        self.sharp_chroma_gen.generate_base_chromatic_scale()
+        assert (self.sharp_chroma_gen.notes ==
+                ["C", "C#", "D", "D#", "E", "F",
+                 "F#", "G", "G#", "A", "A#", "B"])
+
+        self.flat_chroma_gen.generate_base_chromatic_scale()
+        assert (self.flat_chroma_gen.notes ==
+                ["C", "Db", "D", "Eb", "E", "F",
+                 "Gb", "G", "Ab", "A", "Bb", "B"])
+
+    def test_generate_chromatic_scale(self):
+        self.sharp_chroma_gen.generate_chromatic_scale()
+        assert (self.sharp_chroma_gen.notes ==
+                ["D#", "E", "F", "F#", "G", "G#",
+                 "A", "A#", "B", "C", "C#", "D"])
+
+        self.flat_chroma_gen.generate_chromatic_scale()
+        assert (self.flat_chroma_gen.notes ==
+                ["Db", "D", "Eb", "E", "F", "Gb",
+                 "G", "Ab", "A", "Bb", "B", "C"])
